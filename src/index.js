a5176cd671be22162e152f7ca8ffe95650c01ff0
ParkerCatz <tksm.webdesign09@gmail.com>
2021-05-15 11:08:17 +0900

    feat: moduleの作成

--- src/index.js
+++ src/index.js
@@ -1,4 +1,14 @@
 import _ from 'lodash'
+// import { Nijou, NAME } from './utility'
+// import * as utility from './utility'
+// import { NAME as NAME_OF_TOKYO } from './utility'
+import Lion from './utility'
+
+// console.log(Nijou(300))
+// console.log(NAME)
+// console.log(utility.NAME)
+// console.log(NAME_OF_TOKYO)
+console.log(Lion.say())
 
 function component() {
   const element = document.createElement('div')
---
+++ src/utility.js
@@ -0,0 +1,11 @@
+export function Nijou(num) {
+  return num * 2
+}
+
+export const NAME = 'Tokyo'
+
+export default class Lion {
+  static say() {
+    return 'Gaoooo'
+  }
+}
